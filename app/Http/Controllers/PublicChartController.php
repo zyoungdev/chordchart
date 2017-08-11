a607ad7ef87b0c9fbf2054a07677cf4333368358
zyisrad <zy@zyisrad.com>
2017-08-10 17:35:13 -0700

    Minor syntax style corrections

--- app/Http/Controllers/PublicChartController.php
+++ app/Http/Controllers/PublicChartController.php
@@ -21,18 +21,18 @@ class PublicChartController extends Controller
     }
 
     public function show( $hash ) {
-        $chart = PublicChart::where('hash', $hash)->first();
+        $chart = PublicChart::where( 'hash', $hash )->first();
 
         if ( !$chart )
-            return redirect('/')->with([
+            return redirect( '/' )->with([
                 'flash' => 'That chart does not exist. A new chart has been created'
             ]);
 
-        return view('chart.existing', ['chart' => $chart] );
+        return view( 'chart.existing', ['chart' => $chart] );
     }
 
     public function update( PublicChartRequest $request, $hash ) {
-        $chart = PublicChart::where('hash', $hash)->first();
+        $chart = PublicChart::where( 'hash', $hash )->first();
 
         $chart->state = $request->input( 'state' );
         $chart->save();
