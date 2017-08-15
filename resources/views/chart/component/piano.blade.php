85cea0e956e545f2c914e4f3a6e3d96c841e9ac8
z y <zy@zyisrad.com>
2017-08-14 21:35:51 -0700

    Fix piano controls css

--- resources/views/chart/component/piano.blade.php
+++ resources/views/chart/component/piano.blade.php
@@ -9,15 +9,15 @@
                 <span>Note Length</span>
                 <input id="pianoAdditionalNoteLength" class="pianoAdditionalNoteLength" type="range" step="1" min="0" max="3" value="1">
             </label>
+            <div id="pianoSequenceControls" class="pianoSequenceControls">
+                <div class="center-container">
+                    <button id="pianoSub" class="pianoSub button">-</button>
+                    <button id="pianoAdd" class="pianoAdd button">+</button>
+                </div>
+            </div>
         </div>
     </div>
     <div id="pianoSequence" class="pianoSequence sequencer">
-        <div id="pianoSequenceControls" class="pianoSequenceControls">
-            <div class="center-container">
-                <button id="pianoSub" class="pianoSub button">-</button>
-                <button id="pianoAdd" class="pianoAdd button">+</button>
-            </div>
-        </div>
         <div id="pianoSequenceContainer" class="pianoSequenceContainer"></div>
     </div>
 </div>
