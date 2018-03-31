e2c1b9e2440937c1bb77ab33e79033930aecd9ab
z y <zy@zyisrad.com>
2018-03-31 22:14:13 +0000

    Fix mixer alignment

--- resources/views/chart/component/mixer.blade.php
+++ resources/views/chart/component/mixer.blade.php
@@ -1,16 +1,14 @@
 <div id="mixer" class="mixer hide">
-    <div class="mixer-container">
-        <label class="mixer-input-container">
-            <span><h2>Metronome</h2></span>
-            <input id="metronomeVolume" class="metronomeVolume vertical-slider" type="range" step="1" min="0" max="10" value="0" orient="vertical">
-        </label>
-        <label class="mixer-input-container">
-            <span><h2>Piano</h2></span>
-            <input id="pianoVolume" class="pianoVolume vertical-slider" type="range" step="1" min="0" max="20" value="10" orient="vertical">
-        </label>
-        <label class="mixer-input-container">
-            <span><h2>Master</h2></span>
-            <input id="masterVolume" class="masterVolume vertical-slider" type="range" step="1" min="0" max="10" value="10" orient="vertical">
-        </label>
-    </div>
+    <label class="mixer-input-container">
+        <span><h2>Metronome</h2></span>
+        <input id="metronomeVolume" class="metronomeVolume vertical-slider" type="range" step="1" min="0" max="10" value="0" orient="vertical">
+    </label>
+    <label class="mixer-input-container">
+        <span><h2>Piano</h2></span>
+        <input id="pianoVolume" class="pianoVolume vertical-slider" type="range" step="1" min="0" max="20" value="10" orient="vertical">
+    </label>
+    <label class="mixer-input-container">
+        <span><h2>Master</h2></span>
+        <input id="masterVolume" class="masterVolume vertical-slider" type="range" step="1" min="0" max="10" value="10" orient="vertical">
+    </label>
 </div>
